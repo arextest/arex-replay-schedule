f7920b3bf738f24f6f58902756bc6da070940f40
rchen9 <rchen9@trip.com>
2022-09-22 16:58:52 +0800

    bugfix:fix the failure of replayComparisonConfig deserialization

--- src/main/java/com/arextest/replay/schedule/model/config/ReplayComparisonConfig.java
+++ src/main/java/com/arextest/replay/schedule/model/config/ReplayComparisonConfig.java
@@ -1,7 +1,17 @@
 package com.arextest.replay.schedule.model.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,11 +29,37 @@ public class ReplayComparisonConfig {
     private Set<List<String>> exclusionList;
     private Set<List<String>> inclusionList;
 
+    @JsonDeserialize(keyUsing = MapKeyDeserializerUtils.class)
+    @JsonSerialize(keyUsing = MapKeySerializerUtils.class)
     private Map<List<String>, List<String>> referenceMap;
+
+    @JsonDeserialize(keyUsing = MapKeyDeserializerUtils.class)
+    @JsonSerialize(keyUsing = MapKeySerializerUtils.class)
     private Map<List<String>, List<List<String>>> listSortMap;
     private Map<String, List<List<String>>> decompressConfig;
 
     public final boolean checkIgnoreMockMessageType(String type) {
         return ignoreTypeList != null && ignoreTypeList.contains(type);
     }
+
+    private static class MapKeyDeserializerUtils extends KeyDeserializer {
+
+        @Override
+        public Object deserializeKey(String s, DeserializationContext deserializationContext) throws IOException {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(s, new TypeReference<List<String>>() {
+            });
+        }
+    }
+
+    private static class MapKeySerializerUtils extends JsonSerializer<List<String>> {
+
+        @Override
+        public void serialize(List<String> stringList, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String string = objectMapper.writeValueAsString(stringList);
+            jsonGenerator.writeFieldName(string);
+        }
+    }
+
 }
