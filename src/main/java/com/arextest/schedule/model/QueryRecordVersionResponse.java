7ddbf2decfd47b8591548224b7b6f7a197182629
sldu <sldu@trip.com>
2023-03-14 15:10:24 +0800

    feat: add recordVersion

---
+++ src/main/java/com/arextest/schedule/model/QueryRecordVersionResponse.java
@@ -0,0 +1,13 @@
+package com.arextest.schedule.model;
+
+import lombok.Data;
+
+/**
+ * @author: sldu
+ * @date: 2023/3/14 17:58
+ **/
+@Data
+public class QueryRecordVersionResponse {
+    private String appId;
+    private String recordVersion;
+}
