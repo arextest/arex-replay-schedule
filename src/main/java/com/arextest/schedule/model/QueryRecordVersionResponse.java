ca90a7448ea66b900f439f4225244782fb56739c
sldu <sldu@trip.com>
2023-03-14 15:10:24 +0800

    feat: add recordVersion

---
+++ src/main/java/com/arextest/schedule/model/QueryRecordVersionResponse.java
@@ -0,0 +1,21 @@
+package com.arextest.schedule.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author: sldu
+ * @date: 2023/3/14 17:58
+ **/
+@Data
+public class QueryRecordVersionResponse {
+
+    private List<ApplicationInstance> body;
+
+    @Data
+    public static class ApplicationInstance{
+        private String appId;
+        private String recordVersion;
+    }
+}
