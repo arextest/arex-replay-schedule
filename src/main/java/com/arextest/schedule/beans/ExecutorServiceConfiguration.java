65a8e7e191e637b36806d7033afd46d9f2d5e96c
qzmo <qzmo@trip.com>
2023-05-16 14:14:23 +0800

    feat: optimized send pool size

--- src/main/java/com/arextest/schedule/beans/ExecutorServiceConfiguration.java
+++ src/main/java/com/arextest/schedule/beans/ExecutorServiceConfiguration.java
@@ -24,6 +24,8 @@ class ExecutorServiceConfiguration implements Thread.UncaughtExceptionHandler {
     private static final int SEND_QUEUE_MAX_CAPACITY_SIZE = 2000;
     private static final int PRELOAD_QUEUE_MAX_CAPACITY_SIZE = 100;
 
+    private static final int SEND_POOL_SIZE = ExecutorServiceConfiguration.calculateIOPoolThreadPoolSize();
+
     @Bean
     public ExecutorService preloadExecutorService() {
         ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("replay-preload-%d")
@@ -35,19 +37,29 @@ class ExecutorServiceConfiguration implements Thread.UncaughtExceptionHandler {
                 TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(PRELOAD_QUEUE_MAX_CAPACITY_SIZE), threadFactory);
     }
 
+    /**
+     * This bean should be overridden according to the actual implementation of compare service
+     */
     @Bean
     public ExecutorService sendExecutorService() {
         ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("replay-send-%d")
                 .setDaemon(true)
                 .setUncaughtExceptionHandler(this)
                 .build();
-        return new ThreadPoolExecutor(MAXIMUM_POOL_SIZE,
-                MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME,
+        return new ThreadPoolExecutor(SEND_POOL_SIZE, SEND_POOL_SIZE, KEEP_ALIVE_TIME,
                 TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(SEND_QUEUE_MAX_CAPACITY_SIZE), threadFactory);
     }
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
         LOGGER.error("uncaughtException {} ,error :{}", t.getName(), e.getMessage(), e);
+    }
+
+    private static int calculateIOPoolThreadPoolSize() {
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        double targetCPUUtilization = 0.8;
+        double wC = 3.0; // assume wait time is 5 times compute time
+        int optimalThreadPoolSize = (int) Math.ceil(nThreads * targetCPUUtilization * (1 + wC));
+        return optimalThreadPoolSize;
     }
 }
