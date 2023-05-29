b1dd751cea8f662cf4beb924b6889eaeb7b3f9a8
wang_yc <wang_yc@trip.com>
2023-05-29 19:44:49 +0800

    feat: add CompareConfigExtensionService

---
+++ src/main/java/com/arextest/schedule/comparer/impl/DefaultCompareConfigExtensionService.java
@@ -0,0 +1,17 @@
+package com.arextest.schedule.comparer.impl;
+
+import com.arextest.schedule.comparer.CompareConfigExtensionService;
+import com.arextest.schedule.model.ReplayActionItem;
+import com.arextest.schedule.model.config.ReplayComparisonConfig;
+
+/**
+ * @Author wang_yc
+ * @Date 2023/5/25 22:31
+ */
+public class DefaultCompareConfigExtensionService implements CompareConfigExtensionService {
+
+    @Override
+    public void build(ReplayComparisonConfig config, ReplayActionItem actionItem) {
+
+    }
+}
