396ea4daa299139cd663ffe5ba61233c78cd1ad5
qzmo <qzmo@trip.com>
2023-06-09 14:46:19 +0800

    fix: useless post process

--- src/main/java/com/arextest/schedule/service/PlanConsumeService.java
+++ src/main/java/com/arextest/schedule/service/PlanConsumeService.java
@@ -314,7 +314,6 @@ public final class PlanConsumeService {
     }
 
     private int doFixedCaseSave(List<ReplayActionCaseItem> caseItemList) {
-        caseItemPostProcess(caseItemList);
         int size = 0;
         for (int i = 0; i < caseItemList.size(); i++) {
             ReplayActionCaseItem caseItem = caseItemList.get(i);
@@ -330,6 +329,8 @@ public final class PlanConsumeService {
                 size++;
             }
         }
+
+        caseItemPostProcess(caseItemList);
         replayActionCaseItemRepository.save(caseItemList);
         return size;
     }
