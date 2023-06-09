32aa4bd9b43717a4f9c26e88da5e710caa5ea2a5
qzmo <qzmo@trip.com>
2023-06-09 14:03:39 +0800

    chore: null guard

--- src/main/java/com/arextest/schedule/model/converter/ReplayCompareResultConverter.java
+++ src/main/java/com/arextest/schedule/model/converter/ReplayCompareResultConverter.java
@@ -1,13 +1,16 @@
 package com.arextest.schedule.model.converter;
 
+import com.arextest.common.utils.SerializationUtils;
+import com.arextest.diff.model.log.LogEntity;
 import com.arextest.schedule.model.ReplayCompareResult;
-import com.arextest.schedule.model.ReplayPlan;
 import com.arextest.schedule.model.dao.mongodb.ReplayCompareResultCollection;
-import com.arextest.schedule.model.dao.mongodb.ReplayPlanCollection;
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 /**
  * Created by qzmo on 2023/06/05.
@@ -21,7 +24,15 @@ public interface ReplayCompareResultConverter {
     @Mappings({
             @Mapping(target = "id", expression = "java(null)"),
             @Mapping(target = "dataChangeCreateTime", expression = "java(System.currentTimeMillis())"),
-            @Mapping(target = "dataChangeUpdateTime", expression = "java(System.currentTimeMillis())")
+            @Mapping(target = "dataChangeUpdateTime", expression = "java(System.currentTimeMillis())"),
+            @Mapping(target = "dataChangeCreateDate", expression = "java(new java.util.Date())")
     })
     ReplayCompareResultCollection daoFromDto(ReplayCompareResult dto);
+
+    default String map(List<LogEntity> logs) {
+        if (logs == null) {
+            return StringUtils.EMPTY;
+        }
+        return SerializationUtils.useZstdSerializeToBase64(logs.toArray());
+    }
 }
