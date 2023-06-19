30bdec3ba3710119f1892ccf494b938f1cac4526
wildeslam <3160104749@zju.edu.cn>
2023-06-19 11:18:28 +0800

    feat:general dubbo support

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/sender/impl/DubboReplaySender.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/sender/impl/DubboReplaySender.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -111,16 +112,33 @@ public class DubboReplaySender extends AbstractReplaySender {
     public static void loadJar(String jarPath) {
         File jarFile = new File(jarPath);
         Method method = null;
+//        try {
+//            method = WebappClassLoaderBase.class.getDeclaredMethod(ADD_URL_FUN_NAME, URL.class);
+//        } catch (NoSuchMethodException | SecurityException e1) {
+//            LOGGER.error("getDeclaredMethod failed, jarPath:{}, message:{}", jarPath, e1.getMessage());
+//        }
+//        boolean accessible = method.isAccessible();
+//        try {
+//            method.setAccessible(true);
+//            Class<?> clazz = DubboReplaySender.class;
+//            ClassLoader classLoader = clazz.getClassLoader();
+//            URL url = jarFile.toURI().toURL();
+//            method.invoke(classLoader, url);
+//        } catch (Exception e2) {
+//            LOGGER.error("addUrl failed, jarPath:{}, message:{}", jarPath, e2.getMessage());
+//        } finally {
+//            method.setAccessible(accessible);
+//        }
+
         try {
-            method = WebappClassLoaderBase.class.getDeclaredMethod(ADD_URL_FUN_NAME, URL.class);
+            method = URLClassLoader.class.getDeclaredMethod(ADD_URL_FUN_NAME, URL.class);
         } catch (NoSuchMethodException | SecurityException e1) {
             LOGGER.error("getDeclaredMethod failed, jarPath:{}, message:{}", jarPath, e1.getMessage());
         }
         boolean accessible = method.isAccessible();
         try {
             method.setAccessible(true);
-            Class<?> clazz = DubboReplaySender.class;
-            ClassLoader classLoader = clazz.getClassLoader();
+            URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
             URL url = jarFile.toURI().toURL();
             method.invoke(classLoader, url);
         } catch (Exception e2) {
