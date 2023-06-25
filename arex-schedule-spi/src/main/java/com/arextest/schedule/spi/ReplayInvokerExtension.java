6de895cc6f0a0a9c44189bdae8c7de2931277049
wildeslam <3160104749@zju.edu.cn>
2023-06-25 14:26:38 +0800

    feat:general protocol support

--- arex-schedule-spi/src/main/java/com/arextest/schedule/spi/ReplayInvokerExtension.java
+++ arex-schedule-spi/src/main/java/com/arextest/schedule/spi/ReplayInvokerExtension.java
@@ -1,15 +1,17 @@
 package com.arextest.schedule.spi;
 
-import com.arextest.schedule.spi.model.ReplayActionCaseItem;
+import com.arextest.schedule.spi.model.ReplayInvokeRequest;
 import com.arextest.schedule.spi.model.ReplayInvokeResult;
 
-public interface ReplaySenderExtension {
+public interface ReplayInvokerExtension {
     String getName();
+
+    boolean isSupported(String caseType);
 
     /**
      * remote invoke.
      * @param request ReplayActionCaseItem's extension
      * @return replayResult with traceId.
      */
-    ReplayInvokeResult invoke(ReplayActionCaseItem request);
+    ReplayInvokeResult invoke(ReplayInvokeRequest request);
 }
