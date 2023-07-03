7f58c532e5b8ff77b39264ba5690d22e30d9d06f
qzmo <qzmo@trip.com>
2023-07-03 19:37:04 +0800

    fix: cpuratio empty when configuration init

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ExecutorServiceConfiguration.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ExecutorServiceConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.*;
 
 /**
