2b27bffddff5176186fc78a465a87d1ca4065038
qzmo <qzmo@trip.com>
2023-09-25 11:09:02 +0800

    feat: compare post process

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/service/ReplayStorageService.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/service/ReplayStorageService.java
@@ -1,9 +1,12 @@
 package com.arextest.schedule.service;
 
+import com.arextest.schedule.client.HttpWepServiceApiClient;
 import com.arextest.schedule.dao.mongodb.ReplayCompareResultRepositoryImpl;
 import com.arextest.schedule.model.storage.CompareResultDbAggStruct;
 import com.arextest.schedule.model.storage.PostProcessResultRequestType;
 import com.arextest.schedule.model.storage.ResultCodeGroup;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,32 +16,44 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class ReplayStorageService {
     @Resource
     private ReplayCompareResultRepositoryImpl replayCompareResultRepository;
+    @Resource
+    private HttpWepServiceApiClient client;
+    @Value("${arex.storage.postProcess.url}")
+    private String postProcessUrl;
 
     public void postProcessCompareResult(String planId) {
-        List<CompareResultDbAggStruct> dbAgg = replayCompareResultRepository.calculateResultCodeGroup(planId);
-        PostProcessResultRequestType storageReq = new PostProcessResultRequestType();
-        List<ResultCodeGroup> resGroups = new ArrayList<>();
-        storageReq.setResults(resGroups);
+        try {
+            List<CompareResultDbAggStruct> dbAgg = replayCompareResultRepository.calculateResultCodeGroup(planId);
+            PostProcessResultRequestType storageReq = new PostProcessResultRequestType();
+            List<ResultCodeGroup> resGroups = new ArrayList<>();
+            storageReq.setResults(resGroups);
 
-        // convert db agg results to request type
-        Map<Integer, List<CompareResultDbAggStruct>> groupByCodes = dbAgg.stream().collect(Collectors.groupingBy(CompareResultDbAggStruct::getDiffResultCode));
-        for (Map.Entry<Integer, List<CompareResultDbAggStruct>> entry : groupByCodes.entrySet()) {
-            ResultCodeGroup resGroup = new ResultCodeGroup();
-            resGroup.setResultCode(entry.getKey());
-            List<ResultCodeGroup.CategoryGroup> categoryGroups = new ArrayList<>();
-            Map<String, CompareResultDbAggStruct> groupByCategory = entry.getValue().stream().collect(Collectors.toMap(CompareResultDbAggStruct::getCategoryName, i -> i));
+            // convert db agg results to request type
+            Map<Integer, List<CompareResultDbAggStruct>> groupByCodes = dbAgg.stream().collect(Collectors.groupingBy(CompareResultDbAggStruct::getDiffResultCode));
+            for (Map.Entry<Integer, List<CompareResultDbAggStruct>> entry : groupByCodes.entrySet()) {
+                ResultCodeGroup resGroup = new ResultCodeGroup();
+                resGroup.setResultCode(entry.getKey());
+                List<ResultCodeGroup.CategoryGroup> categoryGroups = new ArrayList<>();
+                Map<String, CompareResultDbAggStruct> groupByCategory = entry.getValue().stream().collect(Collectors.toMap(CompareResultDbAggStruct::getCategoryName, i -> i));
 
-            for (Map.Entry<String, CompareResultDbAggStruct> categoryEntry : groupByCategory.entrySet()) {
-                ResultCodeGroup.CategoryGroup categoryGroup = new ResultCodeGroup.CategoryGroup();
-                categoryGroup.setCategoryName(categoryEntry.getKey());
-                categoryGroup.setResultIds(categoryEntry.getValue().getRelatedIds());
-                categoryGroups.add(categoryGroup);
+                for (Map.Entry<String, CompareResultDbAggStruct> categoryEntry : groupByCategory.entrySet()) {
+                    ResultCodeGroup.CategoryGroup categoryGroup = new ResultCodeGroup.CategoryGroup();
+                    categoryGroup.setCategoryName(categoryEntry.getKey());
+                    categoryGroup.setResultIds(categoryEntry.getValue().getRelatedIds());
+                    categoryGroups.add(categoryGroup);
+                }
+                resGroup.setCategoryGroups(categoryGroups);
+                resGroups.add(resGroup);
             }
-            resGroup.setCategoryGroups(categoryGroups);
-            resGroups.add(resGroup);
+
+            String out = client.jsonPost(postProcessUrl, storageReq, String.class);
+            LOGGER.info("postProcessCompareResult result: {}", out);
+        } catch (Exception e) {
+            LOGGER.error("postProcessCompareResult error", e);
         }
     }
 }
