6fb031bafffc71a9e16f5286041eda3dda1a4c07
yushuwang <yushuwang@trip.com>
2023-11-17 15:13:19 +0800

    feat: get pom version at vi/health interface

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/web/controller/CheckHealthController.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/web/controller/CheckHealthController.java
@@ -2,10 +2,8 @@ package com.arextest.schedule.web.controller;
 
 import com.arextest.common.model.response.Response;
 import com.arextest.common.utils.ResponseUtils;
-import java.io.FileReader;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,19 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/vi/")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class CheckHealthController {
-
-  private static final String POM_PATH_TOMCAT = "/usr/local/tomcat/webapps/ROOT/META-INF/maven"
-      + "/com.arextest/arex-schedule-web-api/pom.xml";
-  private static String VERSION;
-
-  static {
-    try {
-      Model pomModel = new MavenXpp3Reader().read(new FileReader(POM_PATH_TOMCAT));
-      VERSION = pomModel.getVersion();
-    } catch (Exception e) {
-      LOGGER.error("Read pom failed!", e);
-    }
-  }
+  @Value("${pom.version}")
+  private String VERSION;
 
   @GetMapping(value = "/health", produces = "application/json")
   @ResponseBody
