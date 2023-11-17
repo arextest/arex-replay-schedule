a77933ab93b4c63bb1d992cabe0a63043e042351
yushuwang <yushuwang@trip.com>
2023-11-17 10:57:13 +0800

    feat: local schedule temp

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/service/LocalReplayService.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/service/LocalReplayService.java
@@ -1,9 +1,27 @@
 package com.arextest.schedule.service;
 
+import com.arextest.schedule.model.plan.BuildReplayPlanRequest;
+import com.arextest.schedule.model.plan.QueryReplayCaseIdResponse;
+import com.arextest.schedule.model.plan.QueryReplayCaseResponse;
+import com.arextest.web.model.contract.contracts.QueryReplayCaseRequestType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 /**
  * @author wildeslam.
  * @create 2023/11/15 17:40
  */
+@Service
+@Slf4j
 public class LocalReplayService {
 
+  public QueryReplayCaseIdResponse queryReplayCaseId(BuildReplayPlanRequest request) {
+
+
+    return new QueryReplayCaseIdResponse();
+  }
+
+  public QueryReplayCaseResponse queryReplayCase(QueryReplayCaseRequestType replayCaseRequestType) {
+    return new QueryReplayCaseResponse();
+  }
 }
--- arex-schedule-web-api/src/main/java/com/arextest/schedule/web/controller/ReplayLocalController.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/web/controller/ReplayLocalController.java
@@ -2,7 +2,10 @@ package com.arextest.schedule.web.controller;
 
 import com.arextest.schedule.model.CommonResponse;
 import com.arextest.schedule.model.plan.BuildReplayPlanRequest;
+import com.arextest.schedule.service.LocalReplayService;
+import com.arextest.web.model.contract.contracts.QueryReplayCaseRequestType;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,15 +22,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path = "/api/replay/local/", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class ReplayLocalController {
 
+  @Autowired
+  private LocalReplayService localReplayService;
+
   @PostMapping(value = "/queryCaseId")
   @ResponseBody
   public CommonResponse queryCaseId(@RequestBody BuildReplayPlanRequest request) {
-    return null;
+    return CommonResponse.successResponse("success", localReplayService.queryReplayCaseId(request));
   }
 
   @PostMapping(value = "/queryCases")
   @ResponseBody
-  public CommonResponse queryCases(@RequestBody BuildReplayPlanRequest request) {
+  public CommonResponse queryCases(@RequestBody QueryReplayCaseRequestType request) {
     return null;
   }
 }
