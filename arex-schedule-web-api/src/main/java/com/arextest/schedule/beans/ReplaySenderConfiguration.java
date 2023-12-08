ea4654b36a52797b109e990db1d325314e774f44
王雨疏 <35320326+wildeslam@users.noreply.github.com>
2023-12-08 10:22:37 +0800

    feat: exclude invokers (#231)
    * feat: exclude invokers
    
    * feat: exclude invokers
    
    * feat: exclude invokers
    
    ---------
    
    Co-authored-by: yushuwang <yushuwang@trip.com>

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ReplaySenderConfiguration.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ReplaySenderConfiguration.java
@@ -1,9 +1,17 @@
 package com.arextest.schedule.beans;
 
+import com.arextest.common.model.classloader.RemoteJarClassLoader;
+import com.arextest.common.utils.RemoteJarLoaderUtils;
+import com.arextest.schedule.common.ClassLoaderUtils;
+import com.arextest.schedule.extension.invoker.ReplayExtensionInvoker;
 import com.arextest.schedule.sender.ReplaySender;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +20,11 @@ import org.springframework.context.annotation.Configuration;
  * @date: 2023/7/19 11:18
  **/
 @Configuration
+@Slf4j
 public class ReplaySenderConfiguration {
+
+  @Value("${replay.sender.extension.jarPath}")
+  private String jarFilePath;
 
   @Bean
   public List<ReplaySender> replaySenderList(List<ReplaySender> replaySenders) {
@@ -20,5 +32,22 @@ public class ReplaySenderConfiguration {
     return replaySenders.stream()
         .sorted(Comparator.comparing(ReplaySender::getOrder, Comparator.reverseOrder()))
         .collect(Collectors.toList());
+  }
+
+  @Bean
+  public List<ReplayExtensionInvoker> invokers() {
+    List<ReplayExtensionInvoker> invokers = new ArrayList<>();
+    if (StringUtils.isEmpty(jarFilePath)) {
+      return invokers;
+    }
+    try {
+      RemoteJarClassLoader classLoader = RemoteJarLoaderUtils.loadJar(jarFilePath);
+      invokers.addAll(RemoteJarLoaderUtils.loadService(ReplayExtensionInvoker.class, classLoader));
+    } catch (Throwable t) {
+      LOGGER.error("Load invoker jar failed, application startup blocked", t);
+      throw new RuntimeException("Load invoker jar failed");
+    }
+    LOGGER.info("Load invoker jar success, invokers: {}", invokers);
+    return invokers;
   }
 }
