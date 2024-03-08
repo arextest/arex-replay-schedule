c01a8da3fa29fd155b83d15ce9664e47f4001d2a
wildeslam <yushuwang@trip.com>
2024-03-08 16:31:25 +0800

    chore:optimize loading invoker.jar

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ReplaySenderConfiguration.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ReplaySenderConfiguration.java
@@ -1,9 +1,13 @@
 package com.arextest.schedule.beans;
 
-import com.arextest.schedule.common.ClassLoaderUtils;
 import com.arextest.schedule.extension.invoker.ReplayExtensionInvoker;
 import com.arextest.schedule.sender.ReplaySender;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -14,6 +18,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author: sldu
@@ -27,6 +32,7 @@ public class ReplaySenderConfiguration {
   private String jarFilePath;
 
   private static final String LOCAL_INVOKER_PATH = "lib/dubboInvoker.jar";
+  private static final String NEW_INVOKER_PATH = "dubboInvoker.jar";
 
   @Bean
   public List<ReplaySender> replaySenderList(List<ReplaySender> replaySenders) {
@@ -40,17 +46,22 @@ public class ReplaySenderConfiguration {
   public List<ReplayExtensionInvoker> invokers() {
     List<ReplayExtensionInvoker> invokers = new ArrayList<>();
 
-
-    if (StringUtils.isNotEmpty(jarFilePath)) {
-      ClassLoaderUtils.loadJar(jarFilePath);
-    } else {
-      // load local invoker
-      URL localInvokerUrl = this.getClass().getClassLoader().getResource(LOCAL_INVOKER_PATH);
-      ClassLoaderUtils.loadJar(localInvokerUrl);
-    }
-
     try {
+      URL classPathResource;
+      if (StringUtils.isEmpty(jarFilePath)) {
+        classPathResource = loadLocalInvokerJar();
+      } else {
+        classPathResource = new File(jarFilePath).toURI().toURL();
+      }
+
+      ClassLoader original = Thread.currentThread().getContextClassLoader();
+      URLClassLoader urlLoader = new URLClassLoader(new URL[]{classPathResource},
+          this.getClass().getClassLoader());
+      Thread.currentThread().setContextClassLoader(urlLoader);
+      Class.forName(
+          "com.arextest.schedule.extension.invoker.ReplayExtensionInvoker", true, urlLoader);
       ServiceLoader.load(ReplayExtensionInvoker.class).forEach(invokers::add);
+      Thread.currentThread().setContextClassLoader(original);
     } catch (Throwable t) {
       LOGGER.error("Load invoker jar failed, application startup blocked", t);
     }
@@ -61,4 +72,27 @@ public class ReplaySenderConfiguration {
     LOGGER.info("Load invoker jar success, invokers: {}", invokers);
     return invokers;
   }
+
+  private void inputStreamToFile(InputStream inputStream, String filePath) throws IOException {
+    File file = new File(filePath);
+    FileOutputStream fos = new FileOutputStream(file);
+    byte[] buffer = new byte[1024];
+    int length;
+    while ((length = inputStream.read(buffer)) > 0) {
+      fos.write(buffer, 0, length);
+    }
+    inputStream.close();
+    fos.close();
+  }
+
+  private URL loadLocalInvokerJar() throws IOException {
+    ClassPathResource classPathResource = new ClassPathResource(LOCAL_INVOKER_PATH);
+    InputStream inputStream = classPathResource.getInputStream();
+    inputStreamToFile(inputStream, NEW_INVOKER_PATH);
+
+    File file = new File(NEW_INVOKER_PATH);
+    return file.toURI().toURL();
+  }
+
+
 }
