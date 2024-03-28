d8b48f1b2b4fe07f2acd8dacfbc2ae4bdf26c615
Rui Chen <44330697+coryhh@users.noreply.github.com>
2024-03-28 16:10:41 +0800

    Feat/supportsential (#285)

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/CacheProviderConfiguration.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/CacheProviderConfiguration.java
@@ -3,6 +3,8 @@ package com.arextest.schedule.beans;
 
 import com.arextest.common.cache.CacheProvider;
 import com.arextest.common.cache.DefaultRedisCacheProvider;
+import com.arextest.common.cache.SentinelRedisCacheProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -14,13 +16,20 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConditionalOnMissingBean(CacheProvider.class)
+@SuppressWarnings("java:S6857")
 class CacheProviderConfiguration {
 
   @Value("${arex.redis.uri}")
   private String scheduleCacheRedisHost;
 
+  @Value("${arex.redis.sentinelUrl:}")
+  private String sentinelUrl;
+
   @Bean
   CacheProvider cacheProvider() {
+    if (StringUtils.isNotEmpty(sentinelUrl)) {
+      return new SentinelRedisCacheProvider(sentinelUrl);
+    }
     return new DefaultRedisCacheProvider(scheduleCacheRedisHost);
   }
 }
