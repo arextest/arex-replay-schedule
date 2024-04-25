e7825faaa003f7e0c26d65beb585975ed66350ab
Rui Chen <44330697+coryhh@users.noreply.github.com>
2024-04-25 15:02:09 +0800

    refactor: improve the AppAuthAspect (#296)

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/aspect/AppAuthAspect.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/aspect/AppAuthAspect.java
@@ -1,31 +1,13 @@
 package com.arextest.schedule.aspect;
 
 import com.arextest.common.annotation.AppAuth;
-import com.arextest.common.context.ArexContext;
-import com.arextest.common.exceptions.ArexException;
-import com.arextest.common.model.response.ResponseCode;
-import com.arextest.common.utils.JwtUtil;
-import com.arextest.common.utils.ResponseUtils;
-import com.arextest.config.model.dao.config.AppCollection;
-import com.arextest.config.model.dao.config.SystemConfigurationCollection;
-import com.arextest.config.model.dao.config.SystemConfigurationCollection.KeySummary;
-import com.arextest.schedule.dao.mongodb.ApplicationRepository;
-import java.util.Optional;
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * @author wildeslam.
@@ -36,81 +18,16 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 public class AppAuthAspect {
 
-  private static final String NO_PERMISSION = "No permission";
-  private static final String NO_APPID = "No appId";
-  private static final String ERROR_APPID = "Error appId";
-  private static Boolean authSwitch;
-
-  @Autowired
-  private ApplicationRepository applicationRepository;
-
   @Resource
-  private MongoTemplate mongoTemplate;
+  AppAuthAspectExecutor appAuthAspectExecutor;
 
-  @Pointcut("@annotation(com.arextest.common.annotation.AppAuth)")
+  @Pointcut(AppAuthAspectExecutor.POINT_CONTENT)
   public void appAuth() {
   }
 
-  @Around("appAuth() && @annotation(auth)")
+  @Around(AppAuthAspectExecutor.AROUND_CONTENT)
   public Object doAround(ProceedingJoinPoint point, AppAuth auth) throws Throwable {
-    if (authSwitch == null) {
-      init();
-    }
-    if (!authSwitch) {
-      return point.proceed();
-    }
-    ArexContext context = ArexContext.getContext();
-    ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-    HttpServletRequest request = requestAttributes.getRequest();
-    String appId = request.getHeader("appId");
-    String accessToken = request.getHeader("access-token");
-    String userName = JwtUtil.getUserName(accessToken);
-    context.setAppId(appId);
-    context.setOperator(userName);
-    if (appId == null) {
-      LOGGER.error("header has no appId");
-      return reject(point, auth, NO_APPID);
-    }
-    AppCollection application = applicationRepository.query(context.getAppId());
-    if (application == null) {
-      LOGGER.error("error appId");
-      return reject(point, auth, ERROR_APPID);
-    }
-    Object result;
-    if (CollectionUtils.isEmpty(application.getOwners()) || application.getOwners()
-        .contains(userName)) {
-      context.setPassAuth(true);
-      result = point.proceed();
-    } else {
-      context.setPassAuth(false);
-      result = reject(point, auth, NO_PERMISSION);
-    }
-    ArexContext.removeContext();
-    return result;
-  }
-
-  private Object reject(ProceedingJoinPoint point, AppAuth auth, String remark) throws Throwable {
-    switch (auth.rejectStrategy()) {
-      case FAIL_RESPONSE:
-        return ResponseUtils.errorResponse(remark, ResponseCode.AUTHENTICATION_FAILED);
-      case DOWNGRADE:
-        ArexContext.getContext().setPassAuth(false);
-      default:
-        return point.proceed();
-    }
-  }
-
-  private void init() {
-    Query query = new Query(
-        Criteria.where(SystemConfigurationCollection.Fields.key).is(KeySummary.AUTH_SWITCH));
-    SystemConfigurationCollection collection = mongoTemplate.findOne(query,
-        SystemConfigurationCollection.class, SystemConfigurationCollection.DOCUMENT_NAME);
-    authSwitch = Optional.ofNullable(collection)
-        .map(SystemConfigurationCollection::getAuthSwitch)
-        .orElse(null);
-    if (authSwitch == null) {
-      throw new ArexException(ResponseCode.AUTHENTICATION_FAILED.getCodeValue(),
-          "get authSwitch failed, please update storage version");    }
+    return appAuthAspectExecutor.doAround(point, auth);
   }
 
 }
--- arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ServiceAutoConfiguration.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/beans/ServiceAutoConfiguration.java
@@ -1,7 +1,11 @@
 package com.arextest.schedule.beans;
 
+import com.arextest.common.jwt.JWTService;
+import com.arextest.common.jwt.JWTServiceImpl;
+import com.arextest.schedule.aspect.AppAuthAspectExecutor;
 import com.arextest.schedule.comparer.ReplayResultComparer;
 import com.arextest.schedule.comparer.impl.PrepareCompareSourceRemoteLoader;
+import com.arextest.schedule.dao.mongodb.ApplicationRepository;
 import com.arextest.schedule.dao.mongodb.ReplayCompareResultRepositoryImpl;
 import com.arextest.schedule.dao.mongodb.ReplayNoiseRepository;
 import com.arextest.schedule.dao.mongodb.ReplayPlanActionRepository;
@@ -9,12 +13,20 @@ import com.arextest.schedule.sender.ReplaySenderFactory;
 import com.arextest.schedule.service.noise.ReplayNoiseIdentify;
 import com.arextest.schedule.service.noise.ReplayNoiseIdentifyService;
 import java.util.concurrent.ExecutorService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class ServiceAutoConfiguration {
+
+  private static final long ACCESS_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;
+  private static final long REFRESH_EXPIRE_TIME = 30 * 24 * 60 * 60 * 1000L;
+
+  @Value("${arex.jwt.secret:arex}")
+  private String tokenSecret;
 
   @Bean
   @ConditionalOnMissingBean(ReplayNoiseIdentify.class)
@@ -32,4 +44,18 @@ public class ServiceAutoConfiguration {
         replayNoiseRepository, replayPlanActionRepository, replaySenderFactory, sourceRemoteLoader,
         sendExecutorService, analysisNoiseExecutorService);
   }
+
+  @Bean
+  @ConditionalOnMissingBean(AppAuthAspectExecutor.class)
+  public AppAuthAspectExecutor appAuthAspectExecutor(ApplicationRepository applicationRepository,
+      MongoTemplate mongoTemplate, JWTService jwtService) {
+    return new AppAuthAspectExecutor(applicationRepository, mongoTemplate, jwtService);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean(JWTService.class)
+  public JWTService jwtService() {
+    return new JWTServiceImpl(ACCESS_EXPIRE_TIME, REFRESH_EXPIRE_TIME, tokenSecret);
+  }
+
 }
