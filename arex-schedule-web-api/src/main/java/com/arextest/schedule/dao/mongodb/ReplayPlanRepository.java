e5caae6e52a2eb41e753a88eb3c85ff45ece0088
dw.meng <dw.meng@trip.com>
2025-01-20 15:38:22 +0800

    feat: ReplayPlan add status test_finish

--- arex-schedule-web-api/src/main/java/com/arextest/schedule/dao/mongodb/ReplayPlanRepository.java
+++ arex-schedule-web-api/src/main/java/com/arextest/schedule/dao/mongodb/ReplayPlanRepository.java
@@ -11,6 +11,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -44,19 +45,12 @@ public class ReplayPlanRepository implements RepositoryField {
     return updateResult.getModifiedCount() > 0;
   }
 
-  public boolean finish(String planId) {
+  public boolean finish(String planId, List<ReplayPlanStageInfo> replayPlanStageList) {
     Query query = Query.query(Criteria.where(DASH_ID).is(planId));
     Update update = MongoHelper.getUpdate();
-    update.set(ReplayPlanCollection.Fields.PLAN_FINISH_TIME, new Date());
-    UpdateResult updateResult = mongoTemplate.updateMulti(query, update,
-        ReplayPlanCollection.class);
-    return updateResult.getModifiedCount() > 0;
-  }
-
-  public boolean updateReplayPlanStageInfo(String planId, List<ReplayPlanStageInfo> replayPlanStageList) {
-    Query query = Query.query(Criteria.where(DASH_ID).is(planId));
-    Update update = MongoHelper.getUpdate();
-    update.set(ReplayPlanCollection.Fields.REPLAY_PLAN_STAGE_LIST, replayPlanStageList);
+    if(CollectionUtils.isNotEmpty(replayPlanStageList)){
+      update.set(ReplayPlanCollection.Fields.REPLAY_PLAN_STAGE_LIST, replayPlanStageList);
+    }
     update.set(ReplayPlanCollection.Fields.PLAN_FINISH_TIME, new Date());
     UpdateResult updateResult = mongoTemplate.updateMulti(query, update,
         ReplayPlanCollection.class);
